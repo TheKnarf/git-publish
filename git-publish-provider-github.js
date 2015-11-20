8cb1ca39e974bcd891687637d1a252d7be4db789
Frank Lyder Bredland <fl@nyggen.com>
2015-11-20 16:09:28 +0100

    First working version

--- git-publish-provider-github.js
+++ git-publish-provider-github.js
@@ -1,1 +1,29 @@
 #!/usr/bin/env node
+
+var GitHubApi = require("github");
+var name = process.argv[2];
+
+if(typeof name == 'undefined') {
+	console.error('Missing argument name');
+	process.exit(1);	
+}
+
+
+var github = new GitHubApi({
+	version: '3.0.0'
+});
+
+github.authenticate({
+	type: 'basic',
+	username: process.env.username,
+	password: process.env.password
+});
+
+github.repos.create({
+	name: name
+}, function(error, result) {
+	if(error != null)
+		process.exit(1);
+
+	console.log(result['clone_url']);
+});
--- git-publish.js
+++ git-publish.js
@@ -1,16 +1,53 @@
 #!/usr/bin/env node
 
-var program = require('commander');
+var program = require('commander'),
+	 exec = require('child_process').exec;
+
+var currentFolderName = require('path').basename(process.cwd());
 
 program
 	.version('0.0.1')
-	.option('-P, --provider [GitHub, Bitbucket, GitLab]', 'Change witch provider to use, defaults to GitHub')
-	.option('-N, --name <name>', 'Name for the newly created repo, defaults to directory name')
-	.option('-U, --user <username>', 'Username for the provider')
+	.description('Create a repo on providers like GitHub, Bitbucket or Gitlab and push to it with a single command')
+	.option('-P, --provider [GitHub, Bitbucket, GitLab]', 'Change witch provider to use, defaults to GitHub', 'github')
+	.option('-N, --name <name>', 'Name for the newly created repo, defaults to directory name', currentFolderName)
+//	.option('-D, --description <desc>', 'Discription for the new repo')
+//	.option('-U, --user <username>', 'Username for login on the selected provider')
+//	.option('-I, --interactive', 'Ask for properties like provider and name interactivly', false)
 
-	
+// TODO: other things to add options for:
+// 		- private / public repo
+// 		- has_issues
+// 		- has wiki
 
 program.parse(process.argv);
 
-//if (!program.args.length) program.help();
+var cmd = 'git-publish-provider-' + program.provider.toLowerCase() + ' ' + program.name;
 
+exec(cmd, function(error, stdout, stderr) {
+	if(error != null) {
+		console.log("Error: ", stderr);
+		process.exit(error);
+	}
+
+	var git_remote = stdout,
+		 cmd_add_remote = 'git remote add origin ' + git_remote,
+		 cmd_git_push = 'git push -u origin $(git symbolic-ref HEAD)';
+
+	console.log(cmd_add_remote);
+	exec(cmd_add_remote, function(error, stdout, stderr) {
+		if(error != null) {
+			console.log("Error: ", stderr);
+			process.exit(error);
+		}
+
+		console.log(cmd_git_push);
+		exec(cmd_git_push, function(error, stdout, stderr) {
+			if(error != null) {
+				console.log("Error: ", stderr);
+				process.exit(error);
+			}
+
+			console.log(stdout);
+		});
+	});
+});
